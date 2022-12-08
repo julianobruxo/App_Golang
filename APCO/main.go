1575e7a531b995571798ff29550c448313220902
julianobruxo <julianobruxo666@gmail.com>
2022-12-08 16:39:01 -0300

    troca de nome do folder

--- APCO/main.go
+++ APCO/main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	app "linha-de-comando/App"
 	"log"
 	"os"
 )
 
 func main() {
-	fmt.Println("Ponto de Parida")
+	fmt.Println("Ponto de Partida")
 
 	aplicacao := app.Gerar()
 	if erro := aplicacao.Run(os.Args); erro != nil {
